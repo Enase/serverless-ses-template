85454fb21dff2c017565bac9c213a27087986302
Sergii Kovalov <enasik@gmail.com>
2023-09-12 15:00:08 -0700

    feat!: jest tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -41,12 +41,8 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
     region: null,
   }
   let logger: SesPluginTypes.ServerlessLogging
-  let logSpy: SesPluginTypes.ServerlessLogging["log"]
-  let writeTextSpy: jest.Mock
-  let progress: SesPluginTypes.ServerlessLogging["progress"]
   let progressUpdateSpy: jest.Mock
   let progressRemoveSpy: jest.Mock
-  let progressCreateSpy: jest.Mock
   let getConfigFileMock: jest.SpyInstance
   let isAutoDeployDisabledMock: jest.SpyInstance
   let serverless: SesPluginTypes.ServerlessExtended
@@ -60,22 +56,26 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
       .spyOn(mockedRuntimeUtils.prototype, "isAutoDeployDisabled")
       .mockImplementation(() => false)
 
-    logSpy = jest.fn() as unknown as SesPluginTypes.ServerlessLogging["log"]
-    writeTextSpy = jest.fn()
     progressUpdateSpy = jest.fn()
     progressRemoveSpy = jest.fn()
-    progressCreateSpy = jest.fn()
-    progress = {
+    const progress = {
       get: jest.fn((_name: string) => ({
         update: progressUpdateSpy,
         remove: progressRemoveSpy,
       })),
-      create: progressCreateSpy,
+      create: jest.fn(),
     } as unknown as SesPluginTypes.ServerlessLogging["progress"]
+
+    const logSpy = {
+      success: jest.fn(),
+      warning: jest.fn(),
+      error: jest.fn(),
+    } as unknown as SesPluginTypes.ServerlessLogging["log"]
+
     logger = {
       log: logSpy,
       progress,
-      writeText: writeTextSpy,
+      writeText: jest.fn(),
     } as SesPluginTypes.ServerlessLogging
     serverless = {
       service: {
@@ -185,6 +185,27 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
     expect(error).toBeInstanceOf(Error)
     expect(error.message).toBe("cannot load")
   })
+  it("should handle unexpected error type when the configuration file cannot be loaded", async () => {
+    jest.mock(
+      "../examples/asset-management/ses-email-templates",
+      () => async () => {
+        throw new Object("cannot load")
+      },
+    )
+    const plugin = new ServerlessSesTemplatePlugin(
+      serverless,
+      options,
+      {} as SesPluginTypes.ServerlessLogging,
+    )
+    const error: Error = await getError(
+      async () => await plugin.loadConfigurationFile(),
+    )
+    expect(serverless.utils.fileExistsSync).toHaveBeenCalledTimes(1)
+    expect(getConfigFileMock).toHaveBeenCalledTimes(1)
+    expect(serverless.classes.Error).toHaveBeenCalledTimes(1)
+    expect(error).toBeInstanceOf(Error)
+    expect(error.message).toBe("cannot load")
+  })
   it("should call syncTemplates method with true argument and return its result when auto deploy is not disabled", async () => {
     const plugin = new ServerlessSesTemplatePlugin(serverless, options, logger)
     plugin.syncTemplates = jest.fn().mockResolvedValueOnce(undefined)
@@ -201,13 +222,13 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
     plugin.syncTemplates = jest.fn().mockResolvedValueOnce(undefined)
     const result = await plugin.syncTemplatesOnDeploy()
     expect(isAutoDeployDisabledMock).toHaveBeenCalledTimes(1)
-    expect(plugin.syncTemplates).toHaveBeenCalledTimes(0)
+    expect(plugin.syncTemplates).not.toHaveBeenCalled()
     expect(result).toBeUndefined()
   })
   it("should update existing templates when they have changed", async () => {
     mockedSesTemplatePluginLogger.mockImplementation(() => {
       const loggerClass = jest.requireActual("../src/logger").default
-      return new loggerClass(logger, logger.writeText, logger.progress)
+      return new loggerClass(logger.log, logger.writeText, logger.progress)
     })
     const addStageToTemplateNameMock = jest
       .spyOn(mockedRuntimeUtils.prototype, "addStageToTemplateName")
@@ -276,7 +297,7 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
   it("should create new templates when they don't exist while deploy", async () => {
     mockedSesTemplatePluginLogger.mockImplementation(() => {
       const loggerClass = jest.requireActual("../src/logger").default
-      return new loggerClass(logger, logger.writeText, logger.progress)
+      return new loggerClass(logger.log, logger.writeText, logger.progress)
     })
 
     const shouldRemoveMissedMock = jest
@@ -320,7 +341,6 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
 
     await plugin.syncTemplates(true)
 
-    // Assert
     expect(logger.progress.create).toHaveBeenCalledWith({
       message: "AWS SES template synchronization",
       name: "sls-ses-template-sync",
@@ -335,7 +355,7 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
     expect(addStageToTemplateNameMock).toHaveBeenCalledWith("test-template")
     expect(logger.progress.get).toHaveBeenCalledWith("sls-ses-template-sync")
     expect(progressRemoveSpy).toHaveBeenCalledTimes(1)
-    expect(writeTextSpy).toHaveBeenCalledTimes(0)
+    expect(logger.writeText).not.toHaveBeenCalled()
     expect(serverless.addServiceOutputSection).toHaveBeenCalledWith(
       "Serverless SES Template",
       expectedOutputSection,
@@ -344,7 +364,7 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
   it("sync templates should delete templates", async () => {
     mockedSesTemplatePluginLogger.mockImplementation(() => {
       const loggerClass = jest.requireActual("../src/logger").default
-      return new loggerClass(logger, logger.writeText, logger.progress)
+      return new loggerClass(logger.log, logger.writeText, logger.progress)
     })
 
     const shouldRemoveMissedMock = jest
@@ -383,9 +403,8 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
       .mockReturnValueOnce(["Deleted templates: del-template-dev"])
     plugin.info = jest.fn().mockResolvedValueOnce(undefined)
 
-    await plugin.syncTemplates(false)
+    await plugin.syncTemplates()
 
-    // Assert
     expect(logger.progress.create).toHaveBeenCalledWith({
       message: "AWS SES template synchronization",
       name: "sls-ses-template-sync",
@@ -393,25 +412,25 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
     expect(plugin.loadConfigurationFile).toHaveBeenCalledTimes(1)
     expect(shouldRemoveMissedMock).toHaveBeenCalledTimes(1)
     expect(plugin.getTemplatesToRemove).toHaveBeenCalledTimes(1)
-    expect(plugin.info).toHaveBeenCalledTimes(0)
+    expect(plugin.info).not.toHaveBeenCalled()
     expect(getEmailTemplateMock).toHaveBeenCalledTimes(1)
     expect(createTemplateMock).toHaveBeenCalledTimes(1)
     expect(addStageToTemplateNameMock).toHaveBeenCalledWith("test-template")
     expect(logger.progress.get).toHaveBeenCalledWith("sls-ses-template-sync")
     expect(progressRemoveSpy).toHaveBeenCalledTimes(1)
     expect(plugin.createSummary).toHaveBeenCalledTimes(3)
-    expect(writeTextSpy).toHaveBeenCalledWith(
+    expect(logger.writeText).toHaveBeenCalledWith(
       `\n${[
         "Created templates: test-template-dev",
         "Deleted templates: del-template-dev",
       ].join("\n")}\n`,
     )
-    expect(serverless.addServiceOutputSection).toHaveBeenCalledTimes(0)
+    expect(serverless.addServiceOutputSection).not.toHaveBeenCalled()
   })
   it("sync templates should return void if no action done", async () => {
     mockedSesTemplatePluginLogger.mockImplementation(() => {
       const loggerClass = jest.requireActual("../src/logger").default
-      return new loggerClass(logger, logger.writeText, logger.progress)
+      return new loggerClass(logger.log, logger.writeText, logger.progress)
     })
 
     const shouldRemoveMissedMock = jest
@@ -440,23 +459,22 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
 
     await plugin.syncTemplates(true)
 
-    // Assert
     expect(logger.progress.create).toHaveBeenCalledWith({
       message: "AWS SES template synchronization",
       name: "sls-ses-template-sync",
     })
     expect(plugin.loadConfigurationFile).toHaveBeenCalledTimes(1)
     expect(shouldRemoveMissedMock).toHaveBeenCalledTimes(1)
-    expect(plugin.getTemplatesToRemove).toHaveBeenCalledTimes(0)
-    expect(plugin.info).toHaveBeenCalledTimes(0)
-    expect(getEmailTemplateMock).toHaveBeenCalledTimes(0)
-    expect(createTemplateMock).toHaveBeenCalledTimes(0)
-    expect(addStageToTemplateNameMock).toHaveBeenCalledTimes(0)
+    expect(plugin.getTemplatesToRemove).not.toHaveBeenCalled()
+    expect(plugin.info).not.toHaveBeenCalled()
+    expect(getEmailTemplateMock).not.toHaveBeenCalled()
+    expect(createTemplateMock).not.toHaveBeenCalled()
+    expect(addStageToTemplateNameMock).not.toHaveBeenCalled()
     expect(logger.progress.get).toHaveBeenCalledWith("sls-ses-template-sync")
     expect(progressRemoveSpy).toHaveBeenCalledTimes(1)
     expect(plugin.createSummary).toHaveBeenCalledTimes(3)
-    expect(writeTextSpy).toHaveBeenCalledTimes(0)
-    expect(serverless.addServiceOutputSection).toHaveBeenCalledTimes(0)
+    expect(logger.writeText).not.toHaveBeenCalled()
+    expect(serverless.addServiceOutputSection).not.toHaveBeenCalled()
   })
   it("should successfully delete an AWS SES template when 'template' parameter is provided", async () => {
     const progressName = "sls-ses-template-delete"
@@ -466,11 +484,7 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
       template: templateName,
       ...options,
     }
-    const plugin = new ServerlessSesTemplatePlugin(
-      serverless,
-      pluginOptions,
-      logger,
-    )
+
     const deleteTemplateSpy = jest
       .spyOn(mockedRequestHandler.prototype, "deleteTemplate")
       .mockResolvedValueOnce(true)
@@ -487,6 +501,11 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
       "logSuccess",
     )
 
+    const plugin = new ServerlessSesTemplatePlugin(
+      serverless,
+      pluginOptions,
+      logger,
+    )
     await plugin.deleteGiven()
 
     expect(createProgressSpy).toHaveBeenCalledWith(
@@ -497,8 +516,45 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
     expect(clearProgressSpy).toHaveBeenCalledWith(progressName)
     expect(logSuccessSpy).toHaveBeenCalledWith(expectedMessage)
   })
+  it("should skipp success message on delete an AWS SES template when no result received", async () => {
+    const progressName = "sls-ses-template-delete"
+    const templateName = "test-template"
+    const deleteTemplateSpy = jest
+      .spyOn(mockedRequestHandler.prototype, "deleteTemplate")
+      .mockResolvedValueOnce(false)
+    const createProgressSpy = jest.spyOn(
+      mockedSesTemplatePluginLogger.prototype,
+      "createProgress",
+    )
+    const clearProgressSpy = jest.spyOn(
+      mockedSesTemplatePluginLogger.prototype,
+      "clearProgress",
+    )
+    const logSuccessSpy = jest.spyOn(
+      mockedSesTemplatePluginLogger.prototype,
+      "logSuccess",
+    )
+    const pluginOptions = {
+      template: templateName,
+      ...options,
+    }
+    const plugin = new ServerlessSesTemplatePlugin(
+      serverless,
+      pluginOptions,
+      logger,
+    )
+
+    await plugin.deleteGiven()
+
+    expect(createProgressSpy).toHaveBeenCalledWith(
+      progressName,
+      "AWS SES template delete",
+    )
+    expect(deleteTemplateSpy).toHaveBeenCalledWith(templateName, progressName)
+    expect(clearProgressSpy).toHaveBeenCalledWith(progressName)
+    expect(logSuccessSpy).not.toHaveBeenCalled()
+  })
   it("should return void when template parameter is not provided", async () => {
-    const plugin = new ServerlessSesTemplatePlugin(serverless, options, logger)
     const deleteTemplateSpy = jest.spyOn(
       mockedRequestHandler.prototype,
       "deleteTemplate",
@@ -508,6 +564,7 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
       "logError",
     )
 
+    const plugin = new ServerlessSesTemplatePlugin(serverless, options, logger)
     await plugin.deleteGiven()
 
     expect(deleteTemplateSpy).not.toHaveBeenCalled()
@@ -585,7 +642,6 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
       .mockImplementation((_condition: boolean, text: string) => text)
     await plugin.info()
 
-    // Assert that the account information is displayed correctly
     expect(getAccountMock).toHaveBeenCalledTimes(1)
     expect(plugin.colorizeText).toHaveBeenCalledTimes(5)
     expect(plugin.colorizeText).toHaveBeenCalledWith(true, "GRANTED")
@@ -606,6 +662,45 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
       ],
     )
   })
+  it("should successfully retrieve account information with default values set", async () => {
+    const accountInfo = {
+      DedicatedIpAutoWarmupEnabled: true,
+      EnforcementStatus: "HEALTHY",
+      ProductionAccessEnabled: true,
+      SendingEnabled: true,
+    }
+
+    const addServiceOutputSectionMock = jest.spyOn(
+      serverless,
+      "addServiceOutputSection",
+    )
+
+    const getAccountMock = jest
+      .spyOn(mockedRequestHandler.prototype, "getAccount")
+      .mockResolvedValueOnce(accountInfo)
+
+    const plugin = new ServerlessSesTemplatePlugin(serverless, options, logger)
+    plugin.colorizeText = jest
+      .fn()
+      .mockImplementation((_condition: boolean, text: string) => text)
+    await plugin.info()
+
+    expect(getAccountMock).toHaveBeenCalledTimes(1)
+    expect(plugin.colorizeText).toHaveBeenCalledTimes(4)
+    expect(plugin.colorizeText).toHaveBeenCalledWith(true, "true")
+    expect(plugin.colorizeText).toHaveBeenCalledWith(true, "true")
+    expect(plugin.colorizeText).toHaveBeenCalledWith(true, "true")
+    expect(plugin.colorizeText).toHaveBeenCalledWith(true, "true")
+    expect(addServiceOutputSectionMock).toHaveBeenCalledWith(
+      "Serverless SES Status",
+      [
+        "Production Access Enabled: true",
+        "Sending Enabled: true",
+        "Dedicated Ip Auto Warmup Enabled: true",
+        "Enforcement Status: HEALTHY",
+      ],
+    )
+  })
   it("should return a string with green color when the condition is true and the text is not empty", () => {
     const greenMock = chalk.green as unknown as jest.Mock
     greenMock.mockReturnValueOnce("test-green")
@@ -616,7 +711,7 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
     const plugin = new ServerlessSesTemplatePlugin(serverless, options, logger)
     const result = plugin.colorizeText(true, "text")
     expect(chalk.green).toHaveBeenCalledTimes(1)
-    expect(chalk.red).toHaveBeenCalledTimes(0)
+    expect(chalk.red).not.toHaveBeenCalled()
     expect(result).toBe("test-green")
   })
   it("should return a string with red color when the condition is false and the text is not empty", () => {
@@ -628,8 +723,102 @@ describe("The `ServerlessSesTemplatePlugin` plugin", () => {
 
     const plugin = new ServerlessSesTemplatePlugin(serverless, options, logger)
     const result = plugin.colorizeText(false, "text")
-    expect(chalk.green).toHaveBeenCalledTimes(0)
+    expect(chalk.green).not.toHaveBeenCalled()
     expect(chalk.red).toHaveBeenCalledTimes(1)
     expect(result).toBe("test-red")
+  })
+  it("should load templates and display them in a table", async () => {
+    mockedSesTemplatePluginLogger.mockImplementation(() => {
+      const loggerClass = jest.requireActual("../src/logger").default
+      return new loggerClass(logger.log, logger.writeText, logger.progress)
+    })
+    const getRegionMock = jest
+      .spyOn(mockedRuntimeUtils.prototype, "getRegion")
+      .mockReturnValueOnce("us-west-2")
+
+    const templates = [
+      {
+        TemplateName: "template1",
+        CreatedTimestamp: new Date(),
+      },
+      {
+        TemplateName: "template2",
+        CreatedTimestamp: new Date(),
+      },
+    ]
+    const loadTemplatesMock = jest
+      .spyOn(mockedRequestHandler.prototype, "loadTemplates")
+      .mockResolvedValueOnce(templates)
+
+    const plugin = new ServerlessSesTemplatePlugin(serverless, options, logger)
+    await plugin.list()
+
+    expect(logger.progress.create).toHaveBeenCalledWith({
+      message: "AWS SES template list for us-west-2 region",
+      name: "sls-ses-template-list",
+    })
+    expect(getRegionMock).toHaveBeenCalledTimes(1)
+    expect(loadTemplatesMock).toHaveBeenCalledTimes(1)
+    expect(logger.writeText).toHaveBeenCalledTimes(1)
+    expect(logger.progress.get).toHaveBeenCalledWith("sls-ses-template-list")
+    expect(progressRemoveSpy).toHaveBeenCalledTimes(1)
+    expect(logger.log.success).toHaveBeenCalledWith(
+      "AWS SES template list finished",
+    )
+  })
+  it("should print warning if no templates loaded", async () => {
+    mockedSesTemplatePluginLogger.mockImplementation(() => {
+      const loggerClass = jest.requireActual("../src/logger").default
+      return new loggerClass(logger.log, logger.writeText, logger.progress)
+    })
+    const getRegionMock = jest
+      .spyOn(mockedRuntimeUtils.prototype, "getRegion")
+      .mockReturnValueOnce("us-west-2")
+
+    const loadTemplatesMock = jest
+      .spyOn(mockedRequestHandler.prototype, "loadTemplates")
+      .mockResolvedValueOnce([])
+
+    const plugin = new ServerlessSesTemplatePlugin(serverless, options, logger)
+    await plugin.list()
+
+    expect(logger.progress.create).toHaveBeenCalledWith({
+      message: "AWS SES template list for us-west-2 region",
+      name: "sls-ses-template-list",
+    })
+    expect(getRegionMock).toHaveBeenCalledTimes(2)
+    expect(loadTemplatesMock).toHaveBeenCalledTimes(1)
+    expect(logger.writeText).not.toHaveBeenCalled()
+    expect(logger.progress.get).toHaveBeenCalledWith("sls-ses-template-list")
+    expect(progressRemoveSpy).toHaveBeenCalledTimes(1)
+    expect(logger.log.success).toHaveBeenCalledWith(
+      "AWS SES template list finished",
+    )
+  })
+  it("should throw if not AWS provider type runtime", async () => {
+    serverless = {
+      ...serverless,
+      service: {
+        provider: {
+          ...serverless.service.provider,
+          name: "unknown",
+        },
+      },
+    } as unknown as SesPluginTypes.ServerlessExtended
+    const pluginOptions = {
+      template: "some-title",
+      ...options,
+    }
+    const plugin = new ServerlessSesTemplatePlugin(
+      serverless,
+      pluginOptions,
+      logger,
+    )
+    const error: Error = await getError(async () => await plugin.deleteGiven())
+    expect(serverless.classes.Error).toHaveBeenCalledTimes(1)
+    expect(error).toBeInstanceOf(Error)
+    expect(error.message).toBe(
+      "@haftahave/serverless-ses-template plugin supports only AWS",
+    )
   })
 })
