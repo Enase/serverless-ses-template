e819b0106443d1e20f4c22f23ecea7353bb9d83d
Sergii Kovalov <enasik@gmail.com>
2023-09-11 18:10:20 -0700

    feat!: jest tests

--- tests/logger.test.ts
+++ tests/logger.test.ts
@@ -2,15 +2,30 @@ import SesTemplatePluginLogger from "../src/logger"
 import type * as SesPluginTypes from "../src/serverless-ses-template-plugin"
 
 describe("The `SesTemplatePluginLogger` class", () => {
-  it("createProgress: should create progress when progress is available", () => {
-    const progressGetSpy = jest.fn()
-    const progressCreateSpy = jest.fn()
-    const logger =
-      jest.fn() as unknown as SesPluginTypes.ServerlessLogging["log"]
-    const slsLogger = new SesTemplatePluginLogger(logger, jest.fn(), {
+  let logger: SesPluginTypes.ServerlessLogging["log"]
+  let progressUpdateSpy: jest.Mock
+  let progressGetSpy: jest.Mock
+  let progressCreateSpy: jest.Mock
+  let progress: SesPluginTypes.ServerlessLogging["progress"]
+
+  beforeEach(() => {
+    logger = {
+      success: jest.fn(),
+      warning: jest.fn(),
+      error: jest.fn(),
+    } as unknown as SesPluginTypes.ServerlessLogging["log"]
+
+    progressUpdateSpy = jest.fn()
+    progressGetSpy = jest.fn((_name) => ({ update: progressUpdateSpy }))
+    progressCreateSpy = jest.fn()
+
+    progress = {
       get: progressGetSpy,
       create: progressCreateSpy,
-    })
+    }
+  })
+  it("createProgress: should create progress when progress is available", () => {
+    const slsLogger = new SesTemplatePluginLogger(logger, jest.fn(), progress)
     slsLogger.createProgress("name", "message")
     expect(progressGetSpy).toHaveBeenCalledTimes(0)
     expect(progressCreateSpy).toHaveBeenCalledWith({
@@ -19,14 +34,6 @@ describe("The `SesTemplatePluginLogger` class", () => {
     })
   })
   it("updateProgress: should update progress when progress is available", () => {
-    const logger =
-      jest.fn() as unknown as SesPluginTypes.ServerlessLogging["log"]
-    const progressUpdateSpy = jest.fn()
-    const progressCreateSpy = jest.fn()
-    const progress = {
-      get: jest.fn((_name) => ({ update: progressUpdateSpy })),
-      create: progressCreateSpy,
-    } as unknown as SesPluginTypes.ServerlessLogging["progress"]
     const slsLogger = new SesTemplatePluginLogger(logger, jest.fn(), progress)
     slsLogger.updateProgress("name", "message")
     expect(progressCreateSpy).toHaveBeenCalledTimes(0)
@@ -36,8 +43,6 @@ describe("The `SesTemplatePluginLogger` class", () => {
   it("clearProgress: should remove progress when progress is available", () => {
     const progressRemoveSpy = jest.fn()
     const progressCreateSpy = jest.fn()
-    const logger =
-      jest.fn() as unknown as SesPluginTypes.ServerlessLogging["log"]
     const progress = {
       get: jest.fn((_name) => ({ remove: progressRemoveSpy })),
       create: progressCreateSpy,
@@ -48,30 +53,18 @@ describe("The `SesTemplatePluginLogger` class", () => {
     expect(progressRemoveSpy).toHaveBeenCalled()
   })
   it("logSuccess: should call the success method of the logger with the provided message", () => {
-    const logger = {
-      success: jest.fn(),
-    } as unknown as SesPluginTypes.ServerlessLogging["log"]
-    const progress = jest.fn() as unknown as SesPluginTypes.ServerlessLogging["progress"]
     const slsLogger = new SesTemplatePluginLogger(logger, jest.fn(), progress)
     const message = "Test message"
     slsLogger.logSuccess(message)
     expect(logger.success).toHaveBeenCalledWith(message)
   })
   it("logWarning: should call the warning method of the log object with a non-empty message", () => {
-    const logger = {
-      warning: jest.fn(),
-    } as unknown as SesPluginTypes.ServerlessLogging["log"]
-    const progress = jest.fn() as unknown as SesPluginTypes.ServerlessLogging["progress"]
     const slsLogger = new SesTemplatePluginLogger(logger, jest.fn(), progress)
     const message = "This is a warning message"
     slsLogger.logWarning(message)
     expect(logger.warning).toHaveBeenCalledWith(message)
   })
   it("logError: should log the error message correctly", () => {
-    const logger = {
-      error: jest.fn(),
-    } as unknown as SesPluginTypes.ServerlessLogging["log"]
-    const progress = jest.fn() as unknown as SesPluginTypes.ServerlessLogging["progress"]
     const slsLogger = new SesTemplatePluginLogger(logger, jest.fn(), progress)
     const errorMessage = "This is an error message"
     slsLogger.logError(errorMessage)
